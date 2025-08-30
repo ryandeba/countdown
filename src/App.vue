533a80ba3b1e256a1645a0c2b3504d503498ac36
Ryan Deba <ryandeba@gmail.com>
2025-08-30 15:37:32 -0600

    close nav dropdown after clicking an option

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
   import { RouterLink, RouterView } from 'vue-router'
+
+  const dropdown = ref(null)
+
+  const closeDropdown = () => {
+    dropdown.value.removeAttribute('open')
+  }
 </script>
 
 <template>
   <div class="navbar bg-base-300 shadow-sm mb-5">
     <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+      <details class="dropdown" ref="dropdown">
+        <summary class="btn btn-ghost btn-circle">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
           </svg>
-        </div>
+        </summary>
+
         <ul
-          tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
         >
           <li>
             <RouterLink
               to="/"
+              @click="closeDropdown"
             >
               Home
             </RouterLink>
@@ -26,6 +34,7 @@
           <li>
             <RouterLink
               to="/letters"
+              @click="closeDropdown"
             >
               Play: Letters
             </RouterLink>
@@ -34,6 +43,7 @@
           <li>
             <RouterLink
               to="/solveLetters"
+              @click="closeDropdown"
             >
               Solve: Letters
             </RouterLink>
@@ -42,12 +52,13 @@
           <li>
             <RouterLink
               to="/solveNumbers"
+              @click="closeDropdown"
             >
               Solve: Numbers
             </RouterLink>
           </li>
         </ul>
-      </div>
+      </details>
     </div>
 
     <div class="navbar-center text-xl font-medium">
